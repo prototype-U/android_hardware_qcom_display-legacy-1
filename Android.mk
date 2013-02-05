09d1de8e1d1fe2a451d19c1742e15c6ad4f32ef2
Konsta <konsta09@gmail.com>
2013-02-05 23:19:37 +0200

    allow to build tilerenderer & makefile cleanup
    Change-Id: I1eb6d19d17ff3cff1f621d8ace6fde7c64850799

--- Android.mk
+++ Android.mk
@@ -1,12 +1,11 @@
 #Enables the listed display HAL modules
 #libs to be built for QCOM targets only
 
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
 
 display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils
-#display-hals += libtilerenderer
+display-hals += libtilerenderer
 
 ifneq ($(TARGET_PROVIDES_LIBLIGHTS),true)
 display-hals += liblight
@@ -14,5 +13,4 @@ endif
 
 include $(call all-named-subdir-makefiles,$(display-hals))
 
-endif
 endif
